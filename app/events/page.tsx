00767848231f2c7c28af77b1219e57ca66b89b0f
L0vKuro <dalmazank7@gmail.com>
2026-08-12 01:12:41 -0700

    Update page.tsx

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -22,7 +22,7 @@ const upcomingEvents = [
     category: 'LAN',
     game: 'CALL OF DUTY',
     featured: true,
-    image: 'event-california-tier-list.jpg',
+    image: 'vintagesocal.png',
     date: 'August 29–30, 2026',
     location: '24331 Muirlands Blvd, Ste D, Lake Forest, CA 92630',
     link: 'https://x.com/ValhallanLF',
